81dc9efe3864294dd9c48ad5aa6e812e1cd574b3
serendipity-harpoon <sunhaopeng2016@gmail.com>
2021-08-24 11:24:23 -0400

    read data from DB

---
+++ db/index.php
@@ -0,0 +1,15 @@
+<?php include_once 'includes/dbh.inc.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>connection with db</title>
+</head>
+<body>
+  <h3>Connect to DB</h3>
+  <?php include_once 'readDB.php';?>
+</body>
+</html>
---
+++ db/readDB.php
@@ -0,0 +1,21 @@
+<?php
+
+$sql = "SELECT * FROM users;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0){
+  while ($row = mysqli_fetch_assoc($result)){
+    echo $row['user_uid']."<br>";
+  }
+}
+
+echo "<br>";
+echo "Another OOP method: ";
+echo "<br>";
+
+$result1 = $conn->query($sql);
+while ($row1 = $result1->fetch_assoc()) {
+    echo $row1["user_uid"]."<br>";
+}
+?>
