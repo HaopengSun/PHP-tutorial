cf78304fde44781363ad1dda5a6db058d18fe86f
HaopengSun <sunhp2016@outlook.com>
2021-09-13 18:39:36 -0400

    login app and initial files

---
+++ login-system/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <title>PHP Login System</title>
+</head>
+
+<body>
+
+<ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="discover.php">About</a></li>
+  <li><a href="blog.php">Find Blogs</a></li>
+  <li><a href="signup.php">Sign Up</a></li>
+  <li><a href="Login.php">Log In</a></li>
+</ul>
